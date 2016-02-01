3d6854c377cdea9759ef15af66a8dd180c9d4023
pleasureinblues <pleasureinblues@gmail.com>
2016-02-01 16:08:43 +0500

    01-02-2016

---
+++ dubilap/local_settings.py
@@ -0,0 +1,23 @@
+"""
+Local Settings
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dubailapdb',
+        'USER': 'dbadmin',
+        'PASSWORD': 'seniorjunior789',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+
+######## EMAIL CONF ########
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'enquiry.dubailap@gmail.com'
+EMAIL_HOST_PASSWORD = 'sonyericssonw200i'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
